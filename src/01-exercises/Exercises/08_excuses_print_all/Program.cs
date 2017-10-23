716349866a8694160540560ce91b78a7d7ad959c
regina508 <regina.olestsenko@tptlive.ee>
2017-10-23 15:37:49 +0300

    ex 8

---
+++ src/01-exercises/Exercises/08_excuses_print_all/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_excuses_print_all
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] excuses = new[] { "Koer sõi kodutöö ära", "Ema ei luba arvutis tiksuda" };
+
+            // use foreach instead
+            for (int i = 0; i < excuses.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}. {excuses[i]}");
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ src/01-exercises/Exercises/ex 6 guess a magic number jackpot/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_6_guess_a_magic_number_jackpot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ma valin ühe suvalise numbri [1-100]. Proovi see ära arvata :)");
+
+            int magicNumber = 57;
+
+            Console.Write("Sina pakud: ");
+
+
+            string input = Console.ReadLine();
+            int value = int.Parse(input);
+
+            if (value > magicNumber)
+            {
+                Console.WriteLine("Minu valitud number on väiksem");
+            }
+
+            if (value < magicNumber)
+            {
+                Console.WriteLine("Minu valitud number on suurem");
+
+            }
+
+            if (value = magicNumber)
+            {
+                Console.WriteLine("Woot! You guessed it!");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue...");
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
