3a14087aa15c06a22a0af2a202429d5b0e0f62fc
regina508 <regina.olestsenko@tptlive.ee>
2017-10-23 14:16:45 +0300

    ex2

---
+++ src/01-exercises/Exercises/ConsoleApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tere");
+            Console.WriteLine();
+            Console.WriteLine("Minu nimi on Regina ning õpin esimest korda programmeerima!");
+            Console.WriteLine();
+            Console.WriteLine("Kindlasti on võimalik midagi rohkemat, kui lihtsalt teksti printimine");
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue");
+            Console.WriteLine();
+
+            Console.ReadLine();
+
+        }
+    }
+}
