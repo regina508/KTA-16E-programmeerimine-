ddf0d9721a6612eed0f6b0f646757bc05eb7d245
regina508 <regina.olestsenko@tptlive.ee>
2017-10-24 22:52:41 +0300

    ex 4 - calculator (nr1+nr2)

--- src/01-exercises/Exercises/04 -Calculator/Program.cs
+++ src/01-exercises/Exercises/04 -Calculator/Program.cs
@@ -14,7 +14,21 @@ namespace _04__Calculator
             Console.WriteLine("Mina olen kalkulaatori programm. Ütle mulle 2 numbrit ja ma ütlen Sulle nende summa");
             Console.WriteLine();
 
-            
+            Console.Write("Esimene number: ");
+            String input1 = Console.ReadLine();
+
+            Console.Write("Teine number: ");
+            String input2 = Console.ReadLine();
+
+            int number1 = int.Parse(input1);
+            int number2 = int.Parse(input2);
+
+            Console.WriteLine("Tulemuseks on: " + (number1 + number2));
+            Console.WriteLine("Press any key to continue");
+
+            Console.ReadLine();
+
+
 
 
             Console.ReadLine();
