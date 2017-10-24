72eecdceed7371c9f7b9dffb99f8ccbc28911932
regina508 <regina.olestsenko@tptlive.ee>
2017-10-24 23:04:49 +0300

    ex5 parse input multiply divide
    korrutis ja jagatis

---
+++ src/01-exercises/Exercises/ex 5 parse input multiply divide/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_5_parse_input_multiply_divide
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mina olen kalkulaatori programm.");
+            Console.WriteLine("Ütle mulle 2 numbrit ja ma ütlen sulle nende korrutise ja jagatise");
+            Console.WriteLine();
+
+            Console.Write("Esimene number: ");
+            String input1 = Console.ReadLine();
+
+            Console.Write("Teine number: ");
+            String input2 = Console.ReadLine();
+
+            int number1 = int.Parse(input1);
+            int number2 = int.Parse(input2);
+
+            Console.WriteLine("Korrutis on: " + (number1 * number2));
+            Console.WriteLine("Jagatis on: " + (number1 / number2));
+
+            Console.WriteLine("Press any key to continue");
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
