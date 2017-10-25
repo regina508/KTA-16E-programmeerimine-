6b4bd3d18a27082c9ec63c3507472f31bc3169b4
regina508 <regina.olestsenko@tptlive.ee>
2017-10-25 12:51:04 +0300

    gradedAssignment

---
+++ src/02-graded-assignment/GradedAssignment/GradedAssignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradedAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("It is a discount calculator. Please enter a sum and i will show price with discount");
+
+            Console.Write("Enter sum: ");
+            String input1 = Console.ReadLine();
+
+            int value = int.Parse(input1);
+
+            if (value > 49)
+                {
+                Console.WriteLine("Your discount is 5%");
+            }
+
+            }
+
+        Console.Readline();
+
+        
+        }
+    }
+}
