a9ce3c4eb6f8ab32a37059452e006f4454f2a169
regina508 <regina.olestsenko@tptlive.ee>
2017-10-25 13:27:16 +0300

    random

--- src/03-dice-roller/03-dice-roller/03.2-dice-roller-random/Program.cs
+++ src/03-dice-roller/03-dice-roller/03.2-dice-roller-random/Program.cs
@@ -10,6 +10,33 @@ namespace _03._2_dice_roller_random
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(" /roll 3d6 2d8");
+            Console.WriteLine();
+
+            Random number = new Random();
+
+            int diceroll = number.Next(1, 6);
+            
+            Console.WriteLine("1d6: " + diceroll);
+
+
+            diceroll = number.Next(1, 6);
+            Console.WriteLine("1d6: ");
+
+            diceroll = number.Next(1, 6);
+            Console.WriteLine("1d6: ");
+
+            diceroll = number.Next(1, 8);
+            Console.WriteLine("1d8: ");
+
+            diceroll = number.Next(1, 8);
+            Console.WriteLine("1d8: ");
+
+
+            Console.WriteLine();
+
+            Console.WriteLine("Roll total: 20");
+
+            Console.ReadLine();
         }
-    }
 }
