65741aaf3b4b95a15fb946d1669ea69aa2d2b1fa
regina508 <regina.olestsenko@tptlive.ee>
2017-10-25 13:10:52 +0300

    dice-roller

--- src/03-dice-roller/03-dice-roller/03-dice-roller/Program.cs
+++ src/03-dice-roller/03-dice-roller/03-dice-roller/Program.cs
@@ -10,6 +10,19 @@ namespace _03_dice_roller
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(" /roll 3d6 2d8");
+            Console.WriteLine();
+
+            Console.WriteLine("1d6: 5");
+            Console.WriteLine("1d6: 3");
+            Console.WriteLine("1d6: 2");
+            Console.WriteLine("1d8: 6");
+            Console.WriteLine("1d8: 4");
+            Console.WriteLine();
+
+            Console.WriteLine("Roll total: 20");
+
+            Console.ReadLine();
         }
     }
 }
