6a404a49a3669b1c75d7f747745ada2fc681fb68
regina508 <regina.olestsenko@tptlive.ee>
2017-11-21 08:38:40 +0200

    04-restaurantReceipt

---
+++ src/04-excercises/RestaurantReceipt/RestaurantReceipt/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantReceipt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Restaurant restaurant = new Restaurant("Taco Palenque", "1200 Main St.");
+
+            Tab tab = new Tab();
+
+            tab.Add(7.99);
+            tab.Add(6.55);
+            tab.Add(2.345);
+
+            Receipt receipt = restaurant.GetReceipt(tab);
+
+            Console.Write(receipt);
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+
+        }
+    }
+}
