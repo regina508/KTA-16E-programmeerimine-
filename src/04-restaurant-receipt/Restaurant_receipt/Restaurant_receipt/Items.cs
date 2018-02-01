5cfa03b0ac0f8b2f27587be01571a5cd4c402ffa
regina508 <regina.olestsenko@tptlive.ee>
2018-02-01 22:32:15 +0200

    Restaurant_receipt

---
+++ src/04-restaurant-receipt/Restaurant_receipt/Restaurant_receipt/Items.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant_receipt
+{
+    class Items
+    {
+        public float Value { get; }
+        public Items(float value)
+        {
+            this.Value = value;
+        }
+    }
+}
---
+++ src/04-restaurant-receipt/Restaurant_receipt/Restaurant_receipt/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant_receipt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Taco Palenque");
+            Console.WriteLine("1200 Main ST.");
+
+            var items = new List<Items>();
+
+            while (true)
+            {
+                Console.Write("Enter price of food item [-5 to quit]:");
+                Console.WriteLine();
+                float input = float.Parse(Console.ReadLine());
+
+                if (input == -5)
+                {
+                    break;
+                }
+                items.Add(new Items(input));
+            }
+
+            var total = items.Sum(x => x.Value);
+            Console.WriteLine($"Subtotal: {total}");
+            Console.WriteLine($"15.00 % Gratuity:{total * 0.15:F2}");
+            Console.WriteLine($"Total: €{total + total * 0.15:F2}");
+            Console.WriteLine("Press any key to continue");
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
